ae595943e244fb533e623c1501ba4968393a3e48
norbert-szyszka15 <norbert.szyszka15@gmail.com>
2025-05-30 18:33:22 +0200

    chore: add files for further expansion

--- src/main/java/org/example/javacalendarwebapp/JavaCalendarWebAppApplication.java
+++ src/main/java/org/example/javacalendarwebapp/JavaCalendarWebAppApplication.java
@@ -2,12 +2,14 @@ package org.example.javacalendarwebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("org.example.projectmanagerapp.entity")
+@EnableJpaRepositories("org.example.javacalendarwebapp.repository")
 public class JavaCalendarWebAppApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(JavaCalendarWebAppApplication.class, args);
     }
-
 }
---
+++ src/main/java/org/example/javacalendarwebapp/entity/Task.java
@@ -0,0 +1,4 @@
+package org.example.javacalendarwebapp.entity;
+
+public class Task {
+}
