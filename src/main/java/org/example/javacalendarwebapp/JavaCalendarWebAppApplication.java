f5a21a9b25762bc5294c5e7d94a3e60c79212d93
norbert-szyszka15 <norbert.szyszka15@gmail.com>
2025-05-30 18:18:40 +0200

    chore: git init and first commit

---
+++ src/main/java/org/example/javacalendarwebapp/JavaCalendarWebAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.javacalendarwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCalendarWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCalendarWebAppApplication.class, args);
+    }
+
+}
